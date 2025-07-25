cf6a399a5bebdd64eb841bbf7822c85c99862036
uduak gabriel <uduakgabriel507@gmail.com>
2025-07-25 09:40:02 -0700

    commit 03

--- LibraryProject/relationship_app/urls.py
+++ LibraryProject/relationship_app/urls.py
@@ -1,17 +1,17 @@
-# # relationship_app/urls.py
+# relationship_app/urls.py
 
-# from django.urls import path
-# from . import views
+from django.urls import path
+from . import views
 
-# app_name = 'relationship_app' # URL namespace for this app
+app_name = 'relationship_app' # URL namespace for this app
 
-# urlpatterns = [
-#     # URL for the function-based view (e.g., /relationship/books/)
-#     path('books/', views.list_books, name='list_books'),
+urlpatterns = [
+    # URL for the function-based view (e.g., /relationship/books/)
+    path('books/', views.list_books, name='list_books'),
 
-#     # URL for the class-based view (e.g., /relationship/library/1/)
-#     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-# ]
+    # URL for the class-based view (e.g., /relationship/library/1/)
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
 from django.urls import path
 from .views import list_books
 from . import views
--- LibraryProject/relationship_app/views.py
+++ LibraryProject/relationship_app/views.py
@@ -1,24 +1,3 @@
-# from django.shortcuts import render
-
-# # Create your views here.
-# relationship_app/views.py
-
-from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Book, Library
-
-# Function-based View: Lists all books
-def list_books(request):
-    books = Book.objects.all().order_by('title')
-    context = {'books': books}
-    return render(request, 'relationship_app/list_books.html', context)
-
-# Class-based View: Displays details for a specific library
-class LibraryDetailView(DetailView):
-    model = Library # Specifies the model this view operates on
-    template_name = 'relationship_app/library_detail.html' # Path to the template
-    context_object_name = 'library' # Name of the object in the template context
-
 from django.shortcuts import render
 from .models import Book 
 from .models import Library
