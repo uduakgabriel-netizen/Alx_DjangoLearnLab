4b9c47180329439de93733316d6ab9abcf8c000e
uduak gabriel <uduakgabriel507@gmail.com>
2025-08-01 07:46:01 -0700

    my first api 01

--- api/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.rest_framework import serializers
-
-
-# class ApiConfig(AppConfig):
-#     default_auto_field = 'django.db.models.BigAutoField'
-#     name = 'api'
-
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+
+
+class BookList(generics.ListAPIView):
+    """
+    API view to list all books.
+    """
+    # The queryset specifies the collection of model instances this view will operate on.
+    # Here, we fetch all Book objects from the database.
+    queryset = Book.objects.all()
+
+    serializer_class = BookSerializer
