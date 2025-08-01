7a1b4cd067f470aa993badc4fc7f929f1a4928d7
uduak gabriel <uduakgabriel507@gmail.com>
2025-08-01 08:50:52 -0700

    my second commit

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,8 +1,24 @@
-from django.urls import path
-from .views import BookList
+# from django.urls import path
+# from .views import BookList
 
-# urlpatterns is a list of URL patterns that Django will use
-# to route incoming requests.
+# # urlpatterns is a list of URL patterns that Django will use
+# # to route incoming requests.
+# urlpatterns = [
+#     path('', BookList.as_view(), name='book-list'),
+# ]
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+
+router = DefaultRouter()
+
+
+router.register(r'books', BookViewSet)
+
+
 urlpatterns = [
-    path('', BookList.as_view(), name='book-list'),
+   
+    path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,15 +1,30 @@
-from rest_framework import generics
+# from rest_framework import generics
+# from .models import Book
+# from .serializers import BookSerializer
+
+
+
+# class BookList(generics.ListAPIView):
+#     """
+#     API view to list all books.
+#     """
+#     # The queryset specifies the collection of model instances this view will operate on.
+#     # Here, we fetch all Book objects from the database.
+#     queryset = Book.objects.all()
+
+#     serializer_class = BookSerializer
+
+# api/views.py
+
+from rest_framework import viewsets
 from .models import Book
-from .serializers import BookSerializer
-
-
-
-class BookList(generics.ListAPIView):
+from .serializers import BookSerialize
+class BookViewSet(viewsets.ModelViewSet):
     """
-    API view to list all books.
+    API endpoint that allows books to be viewed, created, edited, or deleted.
     """
-    # The queryset specifies the collection of model instances this view will operate on.
-    # Here, we fetch all Book objects from the database.
+    
     queryset = Book.objects.all()
-
+    
+    
     serializer_class = BookSerializer
