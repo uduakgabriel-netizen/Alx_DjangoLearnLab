095011db36f5a1dd14ed806e261dbdb1e218538f
uduak gabriel <uduakgabriel507@gmail.com>
2025-08-07 10:55:19 -0700

    commit 002

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-
+# this is my models.py file for the advanced API project
 
 
 class AuthorModel(models.Model):
     name = models.CharField(max_length = 100)
+    
     
     def __str__(self):
         return self.name
