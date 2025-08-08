e32b1496cc98ce6e873ba9f66a477149adb5fb26
uduak gabriel <uduakgabriel507@gmail.com>
2025-08-08 05:53:56 -0700

    my updated file

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from api.views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+from .views import BookListCreateView, BookDetailUpdateDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Include the API URLs
+    path('books/', BookListView.as_view(), name='book-list'),
 ]
