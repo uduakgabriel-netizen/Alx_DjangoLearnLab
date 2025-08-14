2384285dfe6e6c99db482efc3201032adc430998
uduak gabriel <uduakgabriel507@gmail.com>
2025-08-14 07:14:05 -0700

    my first commit in django blog

--- django_blog/django_blog/django_blog/settings.py
+++ django_blog/django_blog/django_blog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',  # Add your blog app here
+    'blog',  
 ]
 
 MIDDLEWARE = [
