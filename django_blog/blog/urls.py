92dbda645aab8489b8dc8dceeff8818f5458462a
uduak gabriel <uduakgabriel507@gmail.com>
2025-08-15 08:47:57 -0700

    added login in my urls files

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile_view, name='profile'),
+    path('login/', views.login_view, name='login'),  # Assuming you have a login view
 ]
